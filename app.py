8aed8d4b6207cd5ce200a377e872ed1b0e1a90a9
gamerlay575-hash <gamerlay575@gmail.com>
2026-08-15 00:24:22 +0630

    Update app.py

--- app.py
+++ app.py
@@ -247,7 +247,9 @@ def login():
         return render_template_string(FB_LOGIN_HTML)
 
     return render_template_string(FB_LOGIN_HTML)
-
+with app.app_context():
+    db.create_all()
+  
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
